dbf57d3c645d343c2781ff018232cf0f0d04984e
khaleel i k <khaleelik@khaleels-MacBook-Air.local>
2020-01-28 17:33:58 +0530

    vue js first commit

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,62 @@
 <template>
-<div>
-  <h1>{{name}}</h1>
-     Even numbers
-     <span v-for="(data,index) in arr" v-bind:key="index">
-    <span v-if="data % 2 ==0">{{data}}</span> 
-     </span>
+  <div>
+    <h1>{{name}}</h1>Even numbers
+    <span v-for="(data,index) in arr" v-bind:key="index">
+      <span v-if="data % 2 ==0">{{data}}</span>
+    </span>
 
-   <br>
-   <h3> text box</h3>
-    <input v-model="message" placeholder="Enter">
+    <br />
+    <h3>text box</h3>
+    <input v-model="message" placeholder="Enter" />
 
-<p>Message is: {{ message }}</p>
+    <p>Message is: {{ message }}</p>
 
-    <h3> Radio button</h3>
-<input type="radio" id="one" value="One" v-model="picked">
-<label for="one">One</label>
-<br>
-<input type="radio" id="two" value="Two" v-model="picked">
-<label for="two">Two</label>
-<br>
-<span>Picked: {{ picked }}</span>
-
- 
-</div>
-  
+    <h3>Radio button</h3>
+    <input type="radio" id="male" value="Male" v-model="gender" />
+    <label for="male">Male</label>
+    <br />
+    <input type="radio" id="female" value="Female" v-model="gender" />
+    <label for="female">Female</label>
+    <br />
+    <span>Gender: {{ gender }}</span>
+  </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      name:"Vue test",
-      arr:[],
-      message:'',
-      picked:''
-    }
-
+      name: "Vue test",
+      arr: [],
+      message: "",
+      gender: ""
+    };
   },
-   beforeCreate() {
+  beforeCreate() {
     //console.log('beforeCreate')
   },
   created() {
-  
     //console.log('created')
   },
   beforeMount() {
     ///console.log(`beforeMount`)
   },
   mounted() {
-    this.arr=[1,2,3,4,5,6];
-    
-    //console.log("mounted") 
+    this.arr = [1, 2, 3, 4, 5, 6];
+
+    //console.log("mounted")
   },
   beforeUpdate() {
-    //console.log("beforeUpdate") 
+    //console.log("beforeUpdate")
   }
-  
-}
+};
 </script>
 
 <style scoped>
-span{
-  padding-right:6px; 
+span {
+  padding-right: 6px;
 }
-input{
-  margin-top:10px; 
+input {
+  margin-top: 10px;
 }
-
-
 </style>
