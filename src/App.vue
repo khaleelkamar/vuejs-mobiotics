4da633b7349a9348ac05aa92aba32c2013437cc6
khaleel i k <khaleelik@khaleels-MacBook-Air.local>
2020-01-28 17:24:21 +0530

    vue js first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <h1>{{name}}</h1>
+     Even numbers
+     <span v-for="(data,index) in arr" v-bind:key="index">
+    <span v-if="data % 2 ==0">{{data}}</span> 
+     </span>
+
+   <br>
+   <h3> text box</h3>
+    <input v-model="message" placeholder="Enter">
+
+<p>Message is: {{ message }}</p>
+
+    <h3> Radio button</h3>
+<input type="radio" id="one" value="One" v-model="picked">
+<label for="one">One</label>
+<br>
+<input type="radio" id="two" value="Two" v-model="picked">
+<label for="two">Two</label>
+<br>
+<span>Picked: {{ picked }}</span>
+
+ 
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      name:"Vue test",
+      arr:[],
+      message:'',
+      picked:''
+    }
+
+  },
+   beforeCreate() {
+    //console.log('beforeCreate')
+  },
+  created() {
+  
+    //console.log('created')
+  },
+  beforeMount() {
+    ///console.log(`beforeMount`)
+  },
+  mounted() {
+    this.arr=[1,2,3,4,5,6];
+    
+    //console.log("mounted") 
+  },
+  beforeUpdate() {
+    //console.log("beforeUpdate") 
   }
+  
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+span{
+  padding-right:6px; 
 }
+input{
+  margin-top:10px; 
+}
+
+
 </style>
